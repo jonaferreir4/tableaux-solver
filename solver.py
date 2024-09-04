992c29c5f0ee97e4a847a5e8bb297726ac128e92
jonaferreir4 <jonaferreira2204@gmail.com>
2024-09-04 11:30:25 -0300

    refatoração

--- solver.py
+++ solver.py
@@ -1,117 +1,145 @@
 from parser import PropositionalFormula, TOKEN_AND, TOKEN_IMPL, TOKEN_NEG, TOKEN_OR
 import sys
 
+# lista de betas que nao foi expandido
 
 class Tableux:
     def __init__(self, formulas):
         self.formulas = formulas
         self.ramo = []
-        self.tamAtual = 0
         self.betas = []
         self.pilhaDeRamos = []
 
-        self.initial_markings = [(formula, True)
-                                 for formula in self.formulas[:-1]]
-        self.initial_markings.append((self.formulas[-1], False))
-        self.ramo.extend(self.initial_markings)
-        self.tamAtual += len(self.ramo)
-        self.betas = [False] * self.tamAtual
 
-    def add_formula_to_branch(self):
-        pass
+        for formula in self.formulas[:-1]: # Passa por todos menos pelo último
+            formula_marcada = (formula, True) # Marca como True
+
+            self.ramo.append(formula_marcada) # Adiciona os fórmulas marcadas como True ao ramo
+            self.betas.append(self.is_beta(formula_marcada)) # Adiciona True ou False ao betas a partir das fórmulas marcadas
+
+        formula_marcada = (self.formulas[-1], False) # Marca o último como False
+
+        self.ramo.append(formula_marcada) # Adiciona a última fórmula marcada como 
+        self.betas.append(self.is_beta(formula_marcada))
+
+    # Função que vai preencher a lista de betas com True se a fórmula for Beta
+    def is_beta(self, formula):
+        conective, subformulas = PropositionalFormula.get_main_conective_and_immediate_subformulas(formula[0])
+
+        if conective == TOKEN_AND and formula[1] == False:
+            return True
+
+        elif conective == TOKEN_OR and formula[1] == True:
+            return True
+
+        elif conective == TOKEN_IMPL and formula[1] == True:
+            return True
+        
+        return False
+
+    # Função para verificar se uma fórmula é um átomo
+    def is_atomo(self, formula):
+        conective, subformulas = PropositionalFormula.get_main_conective_and_immediate_subformulas(formula[0])
+
+        if conective == 'atom':
+            return True
+        
+        return False
 
     def expand_alpha(self):
         i = 0
-        while i < self.tamAtual:
-            conective, subformulas = PropositionalFormula.get_main_conective_and_immediate_subformulas(
-                self.ramo[i][0])
-            if conective == TOKEN_AND and self.ramo[i][1] == True:
-                self.ramo.extend(
-                    [(subformulas[0], True), (subformulas[1], True)])
-                self.tamAtual += 1
-                self.betas.extend([False, False])
-                self.ramo.pop(i)
-                continue
+        while i < len(self.ramo):
+            conective, subformulas = PropositionalFormula.get_main_conective_and_immediate_subformulas(self.ramo[i][0])
 
-            elif conective == TOKEN_OR and self.ramo[i][1] == False:
-                self.ramo.extend(
-                    [(subformulas[0], False), (subformulas[1], False)])
-                self.tamAtual += 1
-                self.betas.extend([False, False])
-                self.ramo.pop(i)
-                continue
+            if conective == TOKEN_AND and self.ramo[i][1] == True:  # TA^B
+                self.ramo.extend([(subformulas[0], True), (subformulas[1], True)]) # Adiciono as subfórmulas no ramo
+                self.betas.extend([self.is_beta((subformulas[0], True)), self.is_beta((subformulas[1], True))])
+                # Adiciona a lista de betas, marcando os betas como True e os alfas como False
 
-            elif conective == TOKEN_IMPL and self.ramo[i][1] == False:
-                self.ramo.extend(
-                    [(subformulas[0], True), (subformulas[1], False)])
-                self.tamAtual += 1
-                self.betas.extend([False, False])
-                self.ramo.pop(i)
-                continue
+                self.ramo.pop(i) # Depois de expandir, eu removo o alfa que foi expandido
+                self.betas.pop(i)
 
-            elif conective == TOKEN_NEG and self.ramo[i][1] == True:
-                self.ramo.append((subformulas[0], False))
-                self.betas.extend([False])
-                self.ramo.pop(i)
-                continue
+            elif conective == TOKEN_OR and self.ramo[i][1] == False: # FAvB
+                self.ramo.extend([(subformulas[0], False), (subformulas[1], False)]) # Adiciono as subfórmulas no ramo
+                self.betas.extend([self.is_beta((subformulas[0], False)), self.is_beta((subformulas[1], False))])
+                # Adiciona a lista de betas, marcando os betas como True e os alfas como False
 
-            elif conective == TOKEN_NEG and self.ramo[i][1] == False:
-                self.ramo.append((subformulas[0], True))
-                self.betas.extend([False])
-                self.ramo.pop(i)
-                continue
+                self.ramo.pop(i) # Depois de expandir, eu removo o alfa que foi expandido
+                self.betas.pop(i) # Depois de remover para ramo tem que remover para betas
 
-            if self.is_branch_closed():
+            elif conective == TOKEN_IMPL and self.ramo[i][1] == False: # FA->B
+                self.ramo.extend([(subformulas[0], True), (subformulas[1], False)]) # Adiciono as subfórmulas no ramo
+                self.betas.extend([self.is_beta((subformulas[0], True)), self.is_beta((subformulas[1], False))])
+                # Adiciona a lista de betas, marcando os betas como True e os alfas como False
+                
+                self.ramo.pop(i) # Depois de expandir, eu removo o alfa que foi expandido
+                self.betas.pop(i) # Depois de remover para ramo tem que remover para betas
+
+            elif conective == TOKEN_NEG and self.ramo[i][1] == True: # T~A
+                self.ramo.append((subformulas[0], False)) # Adiciono as subfórmulas no ramo
+                self.betas.append(self.is_beta((subformulas[0], False)))
+                # Adiciona a lista de betas, marcando os betas como True e os alfas como False
+                
+                self.ramo.pop(i) # Depois de expandir, eu removo o alfa que foi expandido
+                self.betas.pop(i) # Depois de remover para ramo tem que remover para betas
+
+            elif conective == TOKEN_NEG and self.ramo[i][1] == False: # F~A
+                self.ramo.append((subformulas[0], True)) # Adiciono as subfórmulas no ramo
+                self.betas.append(self.is_beta((subformulas[0], True)))
+                # Adiciona a lista de betas, marcando os betas como True e os alfas como False
+                
+                self.ramo.pop(i) # Depois de expandir, eu removo o alfa que foi expandido
+                self.betas.pop(i) # Depois de remover para ramo tem que remover para betas
+            
+            i += 1  # Avançar para a próxima fórmula
+            if self.is_branch_closed()[0]: # se o ramo fechar, não precisa terminar de expandir
                 break
 
-            i += 1  # Avançar para a próxima fórmula
-
     def expand_beta(self):
-        for i in range(self.tamAtual):
-            if not self.betas[i]:
-                conective, subformulas = PropositionalFormula.get_main_conective_and_immediate_subformulas(
-                    self.ramo[i][0])
+        for i in range(len(self.ramo)):
+            if self.betas[i]:
+                conective, subformulas = PropositionalFormula.get_main_conective_and_immediate_subformulas(self.ramo[i][0])
+
                 if conective == TOKEN_AND and self.ramo[i][1] == False:
                     self.ramo.append((subformulas[0], False))
-                    self.tamAtual += 1
-                    self.pilhaDeRamos.extend(
-                        [self.ramo[self.tamAtual - 1], self.tamAtual, self.betas])
-                    self.betas[i] = True
+                    self.betas.append(self.is_beta((subformulas[0], False)))
+                    self.betas[i] = False
+                    self.expand_alpha()  # Sempre que eu expando um beta, eu vou tentar expandir alfa
+                    self.pilhaDeRamos.extend([(subformulas[1], False), len(self.ramo), self.betas.copy()])
 
                 elif conective == TOKEN_OR and self.ramo[i][1] == True:
-                    self.betas[i] = True
-                    self.pilhaDeRamos.extend(
-                        [self.ramo[self.tamAtual - 1], self.tamAtual, self.betas])
                     self.ramo.append((subformulas[0], True))
-                    self.tamAtual += 1
+                    self.betas.append(self.is_beta((subformulas[0], True)))
+                    self.betas[i] = False
+                    self.expand_alpha()  # Sempre que eu expando um beta, eu vou tentar expandir alfa
+                    self.pilhaDeRamos.extend([(subformulas[1], True), len(self.ramo), self.betas.copy()])
 
                 elif conective == TOKEN_IMPL and self.ramo[i][1] == True:
                     self.ramo.append((subformulas[0], False))
-                    self.tamAtual += 1
-                    self.pilhaDeRamos.extend(
-                        [self.ramo[self.tamAtual - 1], self.tamAtual, self.betas])
-                    self.betas[i] = True
+                    self.betas.append(self.is_beta((subformulas[0], False)))
+                    self.betas[i] = False
+                    self.expand_alpha() # Sempre que eu expando um beta, eu vou tentar expandir alfa
+                    self.pilhaDeRamos.extend([(subformulas[1], True), len(self.ramo), self.betas.copy()])
 
-            if self.is_branch_closed()[0]:
-                self.ramo.pop(self.is_branch_closed()[1])
-                self.betas.pop()
-                self.tamAtual -=1
+                
 
+            if self.is_branch_closed() [0]:
                 break
-
-            self.expand_alpha()
 
     def is_branch_closed(self):
         literals = {}
-
-        for i in range(self.tamAtual):
-            if len(self.ramo[i][0]) == 1:
+        for i in range(len(self.ramo)):
+            if self.is_atomo(self.ramo[i][0]):
                 if self.ramo[i][0] in literals:
                     if literals[self.ramo[i][0]] != self.ramo[i][1]:
                         return (True, i)
                 else:
                     literals[self.ramo[i][0]] = self.ramo[i][1]
         return (False, None)
+    
+    def prove(self):
+        self.expand_alpha()
+        self.expand_beta()
 
 
 def main():
@@ -120,11 +148,11 @@ def main():
         num_formulas = int(x.readline())
         formulas = [x.readline().strip() for _ in range(num_formulas)]
         tableux = Tableux(formulas)
-        tableux.expand_alpha()
-        tableux.expand_beta()
+        tableux.prove()
         print(f"RAMO - {tableux.ramo}")
         print(f"BETAS - {tableux.betas}")
-        print(f"TAMANHO DO RAMO - {tableux.tamAtual}")
+        print(f"TAMANHO DO RAMO - {len(tableux.ramo)}")
+        print(f"TAMANHO DO BETA - {len(tableux.betas)}")
         print(f"PILHA - {tableux.pilhaDeRamos}")
 
 
