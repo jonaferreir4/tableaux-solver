bb44f12d783b632f236747f38a616940ee1fa593
jonaferreir4 <jonaferreira2204@gmail.com>
2024-09-05 23:07:56 -0300

    pre-pronto

--- solver.py
+++ solver.py
@@ -1,7 +1,6 @@
 from parser import PropositionalFormula, TOKEN_AND, TOKEN_IMPL, TOKEN_NEG, TOKEN_OR
 import sys
 
-# lista de betas que nao foi expandido
 
 class Tableux:
     def __init__(self, formulas):
@@ -9,7 +8,6 @@ class Tableux:
         self.ramo = []
         self.betas = []
         self.pilhaDeRamos = []
-
 
         for formula in self.formulas[:-1]: # Passa por todos menos pelo último
             formula_marcada = (formula, True) # Marca como True
@@ -21,6 +19,8 @@ class Tableux:
 
         self.ramo.append(formula_marcada) # Adiciona a última fórmula marcada como 
         self.betas.append(self.is_beta(formula_marcada))
+
+        self.expand_alpha()
 
     # Função que vai preencher a lista de betas com True se a fórmula for Beta
     def is_beta(self, formula):
@@ -92,7 +92,8 @@ class Tableux:
                 self.betas.pop(i) # Depois de remover para ramo tem que remover para betas
             
             i += 1  # Avançar para a próxima fórmula
-            if self.is_branch_closed()[0]: # se o ramo fechar, não precisa terminar de expandir
+            
+            if self.is_branch_closed(): # se o ramo fechar, não precisa terminar de expandir
                 break
 
     def expand_beta(self):
@@ -104,43 +105,90 @@ class Tableux:
                     self.ramo.append((subformulas[0], False))
                     self.betas.append(self.is_beta((subformulas[0], False)))
                     self.betas[i] = False
-                    self.expand_alpha()  # Sempre que eu expando um beta, eu vou tentar expandir alfa
-                    self.pilhaDeRamos.extend([(subformulas[1], False), len(self.ramo), self.betas.copy()])
+                    self.pilhaDeRamos.append([(subformulas[1], False), len(self.ramo), self.betas.copy()])
 
                 elif conective == TOKEN_OR and self.ramo[i][1] == True:
                     self.ramo.append((subformulas[0], True))
                     self.betas.append(self.is_beta((subformulas[0], True)))
                     self.betas[i] = False
-                    self.expand_alpha()  # Sempre que eu expando um beta, eu vou tentar expandir alfa
-                    self.pilhaDeRamos.extend([(subformulas[1], True), len(self.ramo), self.betas.copy()])
+                    self.pilhaDeRamos.append([(subformulas[1], True), len(self.ramo), self.betas.copy()])
 
                 elif conective == TOKEN_IMPL and self.ramo[i][1] == True:
                     self.ramo.append((subformulas[0], False))
                     self.betas.append(self.is_beta((subformulas[0], False)))
                     self.betas[i] = False
-                    self.expand_alpha() # Sempre que eu expando um beta, eu vou tentar expandir alfa
-                    self.pilhaDeRamos.extend([(subformulas[1], True), len(self.ramo), self.betas.copy()])
+                    self.pilhaDeRamos.append([(subformulas[1], True), len(self.ramo), self.betas.copy()])
 
-                
-
-            if self.is_branch_closed() [0]:
-                break
+                break           
+    
+    def unstack(self):
+        if not self.pilhaDeRamos:
+            return
+        
+        beta2, tam, betas = self.pilhaDeRamos.pop()
+        self.ramo = self.ramo[:tam]
+        self.betas = betas
+        self.ramo.append(beta2)
+        self.betas.append(self.is_beta(beta2))
 
     def is_branch_closed(self):
-        literals = {}
-        for i in range(len(self.ramo)):
-            if self.is_atomo(self.ramo[i][0]):
-                if self.ramo[i][0] in literals:
-                    if literals[self.ramo[i][0]] != self.ramo[i][1]:
-                        return (True, i)
+        literals = {} # armazena os literais para comparação
+        for i in range(len(self.ramo)): # Vai percorrer todo o ramo
+            if self.is_atomo(self.ramo[i][0]): # Vai limitar os próximos passos aos átomos
+                if self.ramo[i][0] in literals: # Verifica se o átomo atual já está no dicionário
+                    if literals[self.ramo[i][0]] != self.ramo[i][1]: # Caso esteja, ele vai comparar se as valorações são diferentes (True, False)
+                        return True # Caso sejam diferentes, retorna True
                 else:
-                    literals[self.ramo[i][0]] = self.ramo[i][1]
-        return (False, None)
+                    literals[self.ramo[i][0]] = self.ramo[i][1] # Adiciona o átomo e a valoração 
+        return False # Retorna falso se não houver contradição 
     
     def prove(self):
-        self.expand_alpha()
-        self.expand_beta()
+        while True:
+            print(f"RAMO - {self.ramo}")
+            print(f"BETAS - {self.betas}")
+            print(f"TAMANHO DO RAMO - {len(self.ramo)}")
+            print(f"TAMANHO DO BETA - {len(self.betas)}")
+            print(f"PILHA - {self.pilhaDeRamos}")
 
+            if self.is_branch_closed():
+                if self.pilhaDeRamos:
+                    self.unstack()
+                else:
+                    return 'Sequente verdadeiro'
+            else:
+                if True in self.betas:
+                    self.expand_beta()
+                    self.expand_alpha()  
+                else:
+                    valoracoes = set()
+
+                    for item in self.ramo: 
+                        if self.is_atomo(item):
+                            valoracoes.add(item)
+                        
+                    return valoracoes      
+
+        # while not self.is_branch_closed():
+        #     print(f"RAMO - {self.ramo}")
+        #     print(f"BETAS - {self.betas}")
+        #     print(f"TAMANHO DO RAMO - {len(self.ramo)}")
+        #     print(f"TAMANHO DO BETA - {len(self.betas)}")
+        #     print(f"PILHA - {self.pilhaDeRamos}")
+                            
+        #     self.expand_alpha()  # Expandir todas as fórmulas alfa primeiro
+
+        #     if not self.pilhaDeRamos and True not in self.betas:
+        #         return 'VALORAÇÃO QUE REFUTA'
+
+        #     self.expand_beta()
+
+        #     print(f"RAMO - {self.ramo}")
+        #     print(f"BETAS - {self.betas}")
+        #     print(f"TAMANHO DO RAMO - {len(self.ramo)}")
+        #     print(f"TAMANHO DO BETA - {len(self.betas)}")
+        #     print(f"PILHA - {self.pilhaDeRamos}")
+
+        # return 'PROVADO'
 
 def main():
     filename = sys.argv[1]
@@ -148,12 +196,7 @@ def main():
         num_formulas = int(x.readline())
         formulas = [x.readline().strip() for _ in range(num_formulas)]
         tableux = Tableux(formulas)
-        tableux.prove()
-        print(f"RAMO - {tableux.ramo}")
-        print(f"BETAS - {tableux.betas}")
-        print(f"TAMANHO DO RAMO - {len(tableux.ramo)}")
-        print(f"TAMANHO DO BETA - {len(tableux.betas)}")
-        print(f"PILHA - {tableux.pilhaDeRamos}")
+        print(tableux.prove())
 
 
 if __name__ == "__main__":
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,35 +1,3 @@
-# beta
-# criar dois novos ramo pra cada formula (OR, IMP)
-def expand_beta(self):
-        i = 0
-        while i < self.tamAtual:
-            conective, subformulas = PropositionalFormula.get_main_conective_and_immediate_subformulas(
-                self.ramo[i][0])
-            if conective == TOKEN_OR and self.ramo[i][1] == True:
-                # Criar dois novos ramos
-                ramo1 = self.ramo.copy()
-                ramo1.append((subformulas[0], True))
-                ramo2 = self.ramo.copy()
-                ramo2.append((subformulas[1], True))
-                self.pilhaDeRamos.append(ramo1)
-                self.pilhaDeRamos.append(ramo2)
-            elif conective == TOKEN_IMPL and self.ramo[i][1] == True:
-                # Criar dois novos ramos
-                ramo1 = self.ramo.copy()
-                ramo1.append((subformulas[0], False))
-                ramo2 = self.ramo.copy()
-                ramo2.append((subformulas[1], True))
-                self.pilhaDeRamos.append(ramo1)
-                self.pilhaDeRamos.append(ramo2)
-            i += 1
-def is_branch_closed(self):
-        literals = {}
 
-        for i in range(self.tamAtual):
-            if len(self.ramo[i][0]) == 1:
-                if self.ramo[i][0] in literals:
-                    if literals[self.ramo[i][0]] != self.ramo[i][1]:
-                        return True
-                else:
-                    literals[self.ramo[i][0]] = self.ramo[i][1]
-        return False
+
+list.pop()
